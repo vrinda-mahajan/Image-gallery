15537581dc20420d6e041687df6dbac5f5b970ac
vrinda-mahajan <vrindamahajan96@gmail.com>
2024-01-19 01:40:07 +0530

    handeled outside modal click

--- src/components/imagesSection.jsx
+++ src/components/imagesSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import img from "../assets/images/landing.svg";
 import techImg1 from "../assets/images/tech1.svg";
-import closeSq from "../assets/icons/closesquare.svg";
 import Searchbar from "./searchbar";
+import Modal from "./modal";
 
 const categories = [
   "Digital",
@@ -53,112 +53,7 @@ function ImagesSection() {
         </div>
       </div>
       {showModal ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
-            <div className="relative w-auto my-6 mx-auto max-w-[85%]">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between py-2 px-6 bg-[#F5F5F5] rounded-t-xl">
-                  <h3 className="text-[20px] font-medium">Preview ID: 48777</h3>
-                  <button
-                    className="ml-auto"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <img alt="close square" src={closeSq} />
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex">
-                  <img
-                    className="w-2/3 rounded object-cover"
-                    alt="tech img"
-                    src={techImg1}
-                  />
-                  <div className="w-1/3 text-[#3B4043] mx-8">
-                    <h2 className="text[21px] tracking-wide">Download</h2>
-                    <div className="border-[1px] w-[275px] rounded-lg mt-3">
-                      <label
-                        htmlFor="small"
-                        className="py-2 px-[14px] border-b-[1px] flex justify-between"
-                      >
-                        <p>Small</p>
-                        <div className="flex">
-                          <p className="font-bold">640 X 960</p>
-                          <input id="small" type="radio" className="ml-6" />
-                        </div>
-                      </label>
-                      <label
-                        htmlFor="medium"
-                        className="py-2 px-[14px] border-b-[1px] flex justify-between"
-                      >
-                        <p>Medium</p>
-                        <div className="flex">
-                          <p className="font-bold">1920 X 2660</p>
-                          <input id="medium" type="radio" className="ml-6" />
-                        </div>
-                      </label>
-                      <label
-                        htmlFor="big"
-                        className="py-2 px-[14px] border-b-[1px] flex justify-between"
-                      >
-                        <p>Big</p>
-                        <div className="flex">
-                          <p className="font-bold">2400 X 3660</p>
-                          <input id="big" type="radio" className="ml-6" />
-                        </div>
-                      </label>
-                      <label
-                        htmlFor="big"
-                        className="py-2 px-[14px] border-b-[1px] flex justify-between"
-                      >
-                        <p>Big</p>
-                        <div className="flex">
-                          <p className="font-bold">2400 X 3660</p>
-                          <input id="big" type="radio" className="ml-6" />
-                        </div>
-                      </label>
-                        
-                    </div>
-                    <button className="bg-[#4BC34B] w-[275px] rounded-lg text-[11px] font-semibold py-[10px] text-white my-4">Download for free!</button>
-                    <h2 className="text[21px] tracking-wide font-medium">Information</h2>
-                    <div className="mt-3 flex flex-wrap justify-between w-[280px]">
-                      <div className="font-semibold">
-                        <span className="text-[11px] text-[#717579]">User</span>
-                        <p>Josch13</p>
-                      </div>
-                      <div className="font-semibold">
-                        <span className="text-[11px] text-[#717579]">User</span>
-                        <p>Josch13</p>
-                      </div>
-                      <div className="font-semibold">
-                        <span className="text-[11px] text-[#717579]">User</span>
-                        <p>Josch13</p>
-                      </div>
-                      
-                    </div>
-                  </div>
-                </div>
-                {/*footer*/}
-                <div className="flex gap-1 items-center pb-6 px-6 rounded-b-lg">
-                  <span className="font-semibold text-[#3B4043]">Tags: </span>
-                  {[
-                    "Weltraumbilder & bilder",
-                    "Erde bilder & bilder",
-                    "Naturbilder",
-                  ].map((tag) => {
-                    return (
-                      <div className="text-[#767676] rounded-sm text-[11px] px-[7px] py-1 inline-block bg-[#F5F5F5]">
-                        {tag}
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        <Modal setShowModal={setShowModal} />
       ) : null}
     </div>
   );
